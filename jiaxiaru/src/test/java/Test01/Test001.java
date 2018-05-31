bdf7ec73a6ba35c2c1d3aac6a7c555ce22c3d612
jiaxiaru <1403522434@qq.com>
2018-05-31 12:01:24 +0800

    addied

---
+++ jiaxiaru/src/test/java/Test01/Test001.java
@@ -0,0 +1,15 @@
+package Test01;
+import org.testng.annotations.*;
+
+public class Test001 {
+  @BeforeClass
+  public void setUp() {
+	  
+  }
+  @Test
+  public void a() {
+	  System.out.println("123456");
+  }
+	
+
+}
